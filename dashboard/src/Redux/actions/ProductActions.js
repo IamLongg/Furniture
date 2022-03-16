3fa5c43426e85f4ceca678da9b629db6bc6676a6
tuilalong <longnguyen2431@gmail.com>
2022-03-16 23:14:31 +0700

    up dashboard

---
+++ dashboard/src/Redux/actions/ProductActions.js
@@ -0,0 +1,46 @@
+// import axios from "axios";
+// import {
+//   PRODUCT_LIST_FAIL,
+//   PRODUCT_LIST_REQUEST,
+//   PRODUCT_LIST_SUCCESS,
+//   PRODUCT_DETAILS_REQUEST,
+//   PRODUCT_DETAILS_SUCCESS,
+//   PRODUCT_DETAILS_FAIL,
+// } from "../constants/ProductConstants";
+
+// export const listProduct =
+//   (keyword = " ", pageNumber = " ") =>
+//   async (dispatch) => {
+//     try {
+//       dispatch({ type: PRODUCT_LIST_REQUEST });
+//       const { data } = await axios.get(
+//         `/products?keyword=${keyword}&pageNumber=${pageNumber}`
+//       );
+//       console.log(data.data);
+//       dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+//     } catch (error) {
+//       dispatch({
+//         type: PRODUCT_LIST_FAIL,
+//         payload:
+//           error.response && error.response.data.message
+//             ? error.response.data.message
+//             : error.message,
+//       });
+//     }
+//   };
+
+// export const listProductDetails = (id) => async (dispatch) => {
+//   try {
+//     dispatch({ type: PRODUCT_DETAILS_REQUEST });
+//     const { data } = await axios.get(`/products/${id}`);
+//     dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+//   } catch (error) {
+//     dispatch({
+//       type: PRODUCT_DETAILS_FAIL,
+//       payload:
+//         error.response && error.response.data.message
+//           ? error.response.data.message
+//           : error.message,
+//     });
+//   }
+// };
